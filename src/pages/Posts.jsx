b61471771b0b38dab4ea59eabb9c811971afdbbf
Andrey Gritsuk <axa8xa8@gmail.com>
2022-03-31 03:46:43 +0300

    Add select oppotunity to change number of posts on page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PostService from "../API/PostService";
 import PostFilter from "../components/PostFilter";
 import PostForm from "../components/PostForm";
@@ -7,11 +7,12 @@ import MyButton from "../components/UI/button/MyButton";
 import Loader from "../components/UI/Loader/Loader";
 import MyModal from "../components/UI/MyModal/MyModal";
 import Pagination from "../components/UI/pagination/Pagination";
+import MySelect from "../components/UI/select/MySelect";
 import { getPageCount } from "../components/utils/pages";
 import { useFetching } from "../hooks/useFetching";
+import { useObserver } from "../hooks/useObserver";
 // import { usePagination } from "./hooks/usePagination";
 import { usePosts } from "../hooks/usePost";
-
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -22,20 +23,23 @@ function Posts() {
   const [limit, setLimit] = useState(5);
   const [page, setPage] = useState(1);
   // let pagesArray = usePagination(posts.length, limit);
-
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const lastElement = useRef();
+
   const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers["x-total-count"];
     setTotalPages(getPageCount(totalCount, limit));
   });
 
-  console.log(totalPages);
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage(page + 1);
+  });
 
   useEffect(() => {
-    fetchPosts();
-  }, [page]);
+    fetchPosts(limit, page);
+  }, [page, limit]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -64,8 +68,28 @@ function Posts() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue="Количество элементов на странице"
+        options={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: 25, name: "25" },
+          { value: -1, name: "Все посты" },
+        ]}
+      />
       {postError && <h1>error: {postError}</h1>}
-      {isPostLoading ? (
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title={"Titles"}
+      />
+      {/* ! for intersection observer API and dinamic loading posts */}
+      <div ref={lastElement}>
+        <hr />
+      </div>
+      {isPostLoading && (
         <div
           style={{
             display: "flex",
@@ -75,13 +99,8 @@ function Posts() {
         >
           <Loader />
         </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title={"Titles"}
-        />
       )}
+
       <Pagination totalPages={totalPages} page={page} changePage={changePage} />
     </div>
   );
